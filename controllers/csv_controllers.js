2f38ced077252fa85d0b6525f4f69b3c46233b34
shivoyraj <shivamrajtripathi@hotmail.com>
2023-02-12 18:32:06 +0530

    added styling for display csv page

--- controllers/csv_controllers.js
+++ controllers/csv_controllers.js
@@ -4,8 +4,10 @@ const CSV = require('../models/csv');
 exports.getCSVByName = async function (req, res) {
   try {
     const fileData = await CSV.findOne({ filename: req.params.fileName });
+    console.log(fileData)
     return res.render('display_csv', {
-      data: fileData.data
+      data: fileData.data,
+      fileName : fileData.filename.split('.')[0]
     });
   } catch (error) {
     console.error("Error occuered while CSV data : ", error);
