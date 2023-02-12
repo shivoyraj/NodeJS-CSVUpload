2219b06d626c6707fd2d0fc4f3f012f039584513
shivoyraj <shivamrajtripathi@hotmail.com>
2023-02-12 21:37:46 +0530

    exception handler and added flash msg

--- controllers/csv_controllers.js
+++ controllers/csv_controllers.js
@@ -1,6 +1,9 @@
 const CSVToJSON = require('csvtojson');
 const CSV = require('../models/csv');
 
+let errorMsg = "";
+let successMsg = ""; 
+
 exports.getCSVByName = async function (req, res) {
   try {
     const fileData = await CSV.findOne({ filename: req.params.fileName });
@@ -20,23 +23,33 @@ exports.uploadCSV = async function (req, res) {
   const pathOfFile = file.path;
   const nameOfFile = file.name;
 
-  const dataOfFile = await CSVToJSON().fromFile(pathOfFile);
   try {
+    const dataOfFile = await CSVToJSON().fromFile(pathOfFile);
     const csvFileUploaded = await CSV.create({
       filename: nameOfFile,
       data: dataOfFile
     });
     console.log('CSV file uploaded successfully' + csvFileUploaded)
+    errorMsg = ""
+    successMsg = "CSV file uploaded successfully"
   } catch (err) {
-    console.log("Error while posting csv file : " + err)
+    successMsg = ""
+    errorMsg = "Invalid CSV File"
   }
+  const allFilenames = await CSV.find({}, { filename: 1 });
 
-  return res.redirect('back');
+  return res.render('index.ejs', {
+    fileNames: allFilenames,
+    errorMessage : errorMsg,
+    successMessage : successMsg
+  });
 }
 
 exports.home = async function (req, res) {
   const allFilenames = await CSV.find({}, { filename: 1 });
   return res.render('index.ejs', {
-    fileNames: allFilenames
+    fileNames: allFilenames,
+    errorMessage : "",
+    successMessage : ""
   });
 }
